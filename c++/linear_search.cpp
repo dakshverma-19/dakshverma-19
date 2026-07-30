5ec590429ae36058f427e47e8739d259dc1144cb
Daksh Verma <dakshverma1912@gmail.com>
2026-07-30 16:10:34 +0530

    Create linear_search.cpp

---
+++ c++/linear_search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+      int linearsearch( int arr[] , int size , int target ){
+            for(int i= 0 ; i< size ; i++ ){
+                 if( arr[i] == target){ //found
+
+                  return i ;
+                 }
+ 
+
+            }
+
+        return -1;              //not found
+      }
+      
+      int main () {
+         int arr[] = { 1 , 2 ,3 ,4 , 5 , 6 ,7 , 8 };
+         int size = 8 ;
+         int target = 7 ;
+
+
+         cout<< linearsearch (  arr , size ,  target ) << endl ; //call function 
+          return 0 ;
+
+
+
+
+      }
